ce2f2773ad5ee1b563a1cea9cebca1f115527232
sheriffkule <zeljko225@yahoo.com>
2024-02-02 20:37:43 +0100

    rendering elements based on objects

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -3,6 +3,25 @@ const searchBtn = document.getElementById('search-btn');
 
 const movies = [];
 
+const renderMovies = () => {
+    const movieList = document.getElementById('movie-list');
+
+    if (movies.length === 0) {
+        movieList.classList.remove('visible');
+        return;
+    } else {
+        movieList.classList.add('visible');
+    }
+    movieList.innerHTML = '';
+    
+    movies.forEach((movie) => {
+        const movieEl = document.createElement('li');
+        movieEl.textContent = movie.info.title;
+        movieList.append(movieEl);
+    });
+    // movieList.innerHTML = '';
+};
+
 const addMovieHandler = () => {
     const title = document.getElementById('title').value;
     const extraName = document.getElementById('extra-name').value;
@@ -24,7 +43,7 @@ const addMovieHandler = () => {
         id: Math.random()
     };
     movies.push(newMovie);
-    console.log(newMovie);
+    renderMovies();
 };
 
 addMovieBtn.addEventListener('click', addMovieHandler);
