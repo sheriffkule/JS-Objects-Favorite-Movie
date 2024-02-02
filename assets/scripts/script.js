ff386e5eb215a51af936fd26d74766583780a34e
sheriffkule <zeljko225@yahoo.com>
2024-02-02 22:09:22 +0100

    adding the filter functionality

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -3,7 +3,7 @@ const searchBtn = document.getElementById('search-btn');
 
 const movies = [];
 
-const renderMovies = () => {
+const renderMovies = (filter = '') => {
     const movieList = document.getElementById('movie-list');
 
     if (movies.length === 0) {
@@ -14,7 +14,11 @@ const renderMovies = () => {
     }
     movieList.innerHTML = '';
 
-    movies.forEach((movie) => {
+    const filteredMovies = !filter
+        ? movies
+        : movies.filter((movie) => movie.info.title.includes(filter));
+
+    filteredMovies.forEach((movie) => {
         const movieEl = document.createElement('li');
         let text = movie.info.title + ' - ';
         for (const key in movie.info) {
@@ -52,4 +56,10 @@ const addMovieHandler = () => {
     renderMovies();
 };
 
+const searchMovieHandler = () => {
+    const filterTerm = document.getElementById('filter-title').value;
+    renderMovies(filterTerm);
+};
+
 addMovieBtn.addEventListener('click', addMovieHandler);
+searchBtn.addEventListener('click', searchMovieHandler);
